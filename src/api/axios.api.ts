6b9003f56093cf57909d4b41c4f79d275bce7f4b
ruslanabdulazimov <abdulazimov.03@mail.ru>
2025-02-09 21:38:04 +0500

    change url

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getTokenFromLocalStorage } from '../helpers/localtorage.helper';
 
 export const instance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'http://217.114.12.78:3000/api',
   headers: {
     Authorization: `Bearer ${getTokenFromLocalStorage()}`,
   },
