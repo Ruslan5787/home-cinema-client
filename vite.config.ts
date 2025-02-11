c6eb58a9aeec5a3fe1003f49bd3b994e5df23976
Ruslan Abdulazimov <abdulazimov.03@mail.ru>
2025-02-11 16:28:29 +0500

    change vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Принимает соединения с любого адреса
+    port: 5173,
+    strictPort: true,  // Использует именно этот порт
+    hmr: {
+      protocol: 'ws',
+      host: '217.114.12.78', // Можно попробовать заменить на твой IP
+      port: 5173,
+    }
+  }
 })
